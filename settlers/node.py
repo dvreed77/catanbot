70b7d9738b366065646d25e37fa0845ab5e28013
David Reed <david.reed.c@gmail.com>
2015-01-14 19:32:25 -0500

    changed tabs to spaces

--- settlers/node.py
+++ settlers/node.py
@@ -1,19 +1,19 @@
 class Node(object):
-	"""docstring for Node"""
-	id_ = 0
-	def __init__(self, arg):
-		super(Node, self).__init__()
-		self.id = Node.id_
-		Node.id_ += 1
+    """docstring for Node"""
+    id_ = 0
+    def __init__(self, arg):
+        super(Node, self).__init__()
+        self.id = Node.id_
+        Node.id_ += 1
 
-		self.node_neighbors = []
-		self.edge_neighbors = []
-		self.face_neighbors = []
-		self.building_type = None
-		self.owner = None
+        self.node_neighbors = []
+        self.edge_neighbors = []
+        self.face_neighbors = []
+        self.building_type = None
+        self.owner = None
 
-	def set_building(self, building_type):
-		self.building_type = building_type
+    def set_building(self, building_type):
+        self.building_type = building_type
 
-	def set_owner(self, player):
-		self.owner = player		
+    def set_owner(self, player):
+        self.owner = player     
