5fe82d6ddd8b7b2daef07253776f99a566f8e577
David Reed <david.reed.c@gmail.com>
2015-01-14 19:11:07 -0500

    created face.py

---
+++ settlers/face.py
@@ -0,0 +1,17 @@
+class Face(object):
+	"""docstring for Face"""
+	id_ = 0
+	def __init__(self, arg):
+		super(Face, self).__init__()
+		self.id = Face.id_
+		Face.id_ += 1
+
+		self.node_neighbors = []
+		self.edge_neighbors = []
+		self.face_neighbors = []
+		self.resource = None
+		self.probability = None
+		self.robber_state = False
+
+	def set_robber(self, state):
+		self.robber = state		
