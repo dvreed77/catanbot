d20019432c6fe9b7b18dc2ed4a86fc4da0caddf0
David Reed <david.reed.c@gmail.com>
2015-01-14 19:31:58 -0500

    changed tabs to spaces

--- settlers/board.py
+++ settlers/board.py
@@ -4,13 +4,13 @@ from settlers.node import Node
 
 
 class Board(object):
-	"""docstring for Board"""
-	def __init__(self, arg):
-		super(Board, self).__init__()
+    """docstring for Board"""
+    def __init__(self, arg):
+        super(Board, self).__init__()
 
-		# TODO: replace with real code
-		self.nodes = [Node(), Node()]
-		self.edges = [Edge(), Edge()]
-		self.faces = [Face(), Face()]
+        # TODO: replace with real code
+        self.nodes = [Node(), Node()]
+        self.edges = [Edge(), Edge()]
+        self.faces = [Face(), Face()]
 
-		
+        
--- settlers/face.py
+++ settlers/face.py
@@ -1,17 +1,17 @@
 class Face(object):
-	"""docstring for Face"""
-	id_ = 0
-	def __init__(self, arg):
-		super(Face, self).__init__()
-		self.id = Face.id_
-		Face.id_ += 1
+    """docstring for Face"""
+    id_ = 0
+    def __init__(self, arg):
+        super(Face, self).__init__()
+        self.id = Face.id_
+        Face.id_ += 1
 
-		self.node_neighbors = []
-		self.edge_neighbors = []
-		self.face_neighbors = []
-		self.resource = None
-		self.probability = None
-		self.robber_state = False
+        self.node_neighbors = []
+        self.edge_neighbors = []
+        self.face_neighbors = []
+        self.resource = None
+        self.probability = None
+        self.robber_state = False
 
-	def set_robber(self, state):
-		self.robber = state		
+    def set_robber(self, state):
+        self.robber = state     
